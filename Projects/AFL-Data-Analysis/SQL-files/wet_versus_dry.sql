4a0968f066ca916f8b33c16229520b82e4be204a
Rahul Chitkara <rahulchitkara2001@gmail.com>
2025-03-12 17:46:57 +1100

    Update wet_versus_dry.sql

--- Projects/AFL-Data-Analysis/SQL-files/wet_versus_dry.sql
+++ Projects/AFL-Data-Analysis/SQL-files/wet_versus_dry.sql
@@ -1,24 +1,24 @@
 USE afl_db;
 
 WITH WetDry_tg AS (
-				SELECT 	 team, venue, rainfall, result,
-						 CASE
-							WHEN rainfall > 0 AND venue != 'Docklands' THEN 'Wet'
-							WHEN rainfall = 0 OR venue = 'Docklands' THEN 'Dry'
-						 END AS WetDry 
-				FROM 	 team_games
+		SELECT 	 team, venue, rainfall, result,
+				 CASE
+					WHEN rainfall > 0 AND venue != 'Docklands' THEN 'Wet'
+					WHEN rainfall = 0 OR venue = 'Docklands' THEN 'Dry'
+				 END AS WetDry 
+		FROM 	 team_games
 ),
 	    win_wd AS (
-				SELECT 	 team, wetdry,
-						 ROUND((COUNT(CASE WHEN result = 'W' THEN 1 ELSE NULL END) / COUNT(*)) * 100, 1) as Win
-				FROM	 wetdry_tg
+		SELECT 	 team, wetdry,
+				 ROUND((COUNT(CASE WHEN result = 'W' THEN 1 ELSE NULL END) / COUNT(*)) * 100, 1) as Win
+		FROM	 wetdry_tg
                 GROUP BY team, wetdry
 ),
        win_wet AS (
-				SELECT 	 wwd1.team, 
-						 wwd1.win - wwd2.win as WetWin
-				FROM 	 win_wd wwd1 INNER JOIN win_wd wwd2
-						 ON wwd1.team = wwd2.team AND wwd1.wetdry > wwd2.wetdry
+		SELECT 	 wwd1.team, 
+				 wwd1.win - wwd2.win as WetWin
+		FROM 	 win_wd wwd1 INNER JOIN win_wd wwd2
+				 ON wwd1.team = wwd2.team AND wwd1.wetdry > wwd2.wetdry
 )
 SELECT 	 team, wetwin,
 		 RANK() OVER(ORDER BY wetwin DESC) as wet_win_rank
