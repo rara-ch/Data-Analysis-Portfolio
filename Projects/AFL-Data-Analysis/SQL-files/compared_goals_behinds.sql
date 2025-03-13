e673be62c5aecf46bd8ece0c7142a92c531be2cc
Rahul Chitkara <rahulchitkara2001@gmail.com>
2025-03-13 11:17:47 +1100

    Update compared_goals_behinds.sql

--- Projects/AFL-Data-Analysis/SQL-files/compared_goals_behinds.sql
+++ Projects/AFL-Data-Analysis/SQL-files/compared_goals_behinds.sql
@@ -1,19 +1,19 @@
 USE afl_db;
 
 WITH  scores AS (
-			SELECT 	 GameID,
-					 CAST(SUBSTRING_INDEX(HomeTeamScoreFT, '.', 1) AS UNSIGNED) as HomeGoals,
-					 CAST(SUBSTRING_INDEX(HomeTeamScoreFT, '.', -1) AS UNSIGNED) as HomeBehinds,
-					 CAST(SUBSTRING_INDEX(AwayTeamScoreFT, '.', 1) AS UNSIGNED) as AwayGoals,
-					 CAST(SUBSTRING_INDEX(HomeTeamScoreFT, '.', -1) AS UNSIGNED) as AwayBehinds
-			FROM 	 games
+		SELECT 	 GameID,
+			 CAST(SUBSTRING_INDEX(HomeTeamScoreFT, '.', 1) AS UNSIGNED) as HomeGoals,
+			 CAST(SUBSTRING_INDEX(HomeTeamScoreFT, '.', -1) AS UNSIGNED) as HomeBehinds,
+			 CAST(SUBSTRING_INDEX(AwayTeamScoreFT, '.', 1) AS UNSIGNED) as AwayGoals,
+			 CAST(SUBSTRING_INDEX(HomeTeamScoreFT, '.', -1) AS UNSIGNED) as AwayBehinds
+		FROM 	 games
 ),
 total_scores AS (
-			SELECT 	 GameID,
-					 HomeGoals + AwayGoals as Goals,
-					 AwayBehinds + AwayBehinds as Behinds,
-					 HomeGoals + AwayGoals + AwayBehinds + AwayBehinds as TotalScores
-			FROM 	 scores
+		SELECT 	 GameID,
+			 HomeGoals + AwayGoals as Goals,
+			 AwayBehinds + AwayBehinds as Behinds,
+			 HomeGoals + AwayGoals + AwayBehinds + AwayBehinds as TotalScores
+		FROM 	 scores
 )
 SELECT 	 ROUND((SUM(Goals)/SUM(TotalScores)) * 100 , 2) as PercentageGoals,
 		 ROUND((SUM(Behinds)/SUM(TotalScores)) * 100 , 2) as PercentageBehinds
