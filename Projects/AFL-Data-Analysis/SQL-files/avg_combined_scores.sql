64e183e695766149156c78c42af142da4bd8ba30
Rahul Chitkara <rahulchitkara2001@gmail.com>
2025-03-13 11:12:37 +1100

    Add files via upload

---
+++ Projects/AFL-Data-Analysis/SQL-files/avg_combined_scores.sql
@@ -0,0 +1,12 @@
+USE afl_db;
+
+WITH combined_scores AS (
+					SELECT 	 GameID, Year,
+							 HomeTeamScore + AwayTeamScore as CombinedScore
+					FROM 	 games
+)
+SELECT 	 year,
+		 ROUND(AVG(CombinedScore)) as AvgCombinedScore
+FROM 	 combined_scores
+GROUP BY year
+ORDER BY year;
---
+++ Projects/AFL-Data-Analysis/SQL-files/avg_team_tackles_2024.sql
@@ -0,0 +1,18 @@
+USE afl_db;
+
+WITH tackles_per_game AS (
+					SELECT	 GameId, Team,
+							 SUM(Tackles) as GameTackles
+					FROM	 stats
+					WHERE 	 year = 2024 AND GameID NOT LIKE '%F%'
+					GROUP BY GameId, Team
+),
+avg_tackles_team AS (
+					SELECT	 Team,
+							 ROUND(AVG(GameTackles)) AS AverageTackles
+					FROM 	 tackles_per_game
+					GROUP BY Team
+)
+SELECT 	 Team, AverageTackles,
+		 ROW_NUMBER() OVER(ORDER BY AverageTackles DESC) as SeasonTacklesRank
+FROM 	 avg_tackles_team;
